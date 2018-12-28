d435dacd8cfec16c768969c928b3ad072ca5ed01
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2018-12-28 09:21:14 +0300

    added Counter

---
+++ Assets/Scripts/UserInterface/CounterView.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+    public class CounterView : MonoBehaviour
+    {
+        public TextMeshProUGUI TextMeshProUGUIText;
+        public Image BackgroundImage;
+    }
+}
