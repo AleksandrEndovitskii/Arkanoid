4aabfff852b9585f28097ca63f1c5bd3c8691e7e
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2018-12-28 08:11:05 +0300

    added Brick Health

--- Assets/Scripts/GameObjects/Brick/BrickView.cs
+++ Assets/Scripts/GameObjects/Brick/BrickView.cs
@@ -1,13 +1,37 @@
-﻿using UnityEngine;
-
-/*
-в зависимости от вида требуется k попаданий шариком для уничтожения кирпича.
- */
+﻿using Managers;
+using UnityEngine;
 
 namespace GameObjects.Brick
 {
     public class BrickView : MonoBehaviour
     {
         public BrickType BrickType;
+
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                _health = value;
+
+                if (_health <= 0)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+
+        private int _initialHealth;
+        private int _health;
+
+        public void Initialize(BrickType brickType)
+        {
+            BrickType = brickType;
+            _initialHealth = GameManager.Instance.BrickHealthManager.GetHealthForBrickType(BrickType);
+            Health = _initialHealth;
+        }
     }
 }
