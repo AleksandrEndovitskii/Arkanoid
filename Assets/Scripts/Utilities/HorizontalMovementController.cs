0f9a74e5b1fb13727fd878c104f9d6a2ab2ead14
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-01-27 01:41:50 +0300

    added horizontal movement controller to Racket

---
+++ Assets/Scripts/Utilities/HorizontalMovementController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public class HorizontalMovementController : MonoBehaviour
+    {
+        float _speed = 10.0f;
+
+        private void Update()
+        {
+            var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            transform.position += move * _speed * Time.deltaTime;
+        }
+    }
+}
