545f1c23408728ba28de8e4626216650f338cb87
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-01-27 03:11:04 +0300

    added instantiating of Bricks of all BrickTypes

--- Assets/Scripts/Managers/GameObjectsManager.cs
+++ Assets/Scripts/Managers/GameObjectsManager.cs
@@ -1,4 +1,7 @@
-﻿using GameObjects.Ball;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameObjects.Ball;
 using GameObjects.Brick;
 using GameObjects.Field;
 using GameObjects.Racket;
@@ -19,16 +22,26 @@ namespace Managers
 
         private BricksContainerView _bricksContainerInstance;
         private FieldView _fieldViewInstance;
-        private BrickView _brickViewInstance;
+        private List<BrickView> _brickViewInstances = new List<BrickView>();
         private RacketView _racketViewInstance;
         private BallView _ballViewInstance;
 
         public void Initialize()
         {
+            var bricksCountPerType = 10;
+
             _fieldViewInstance = InstantiateElement<FieldView>(FieldViewPrefab, GameObjectsContainer);
 
-            _bricksContainerInstance = InstantiateElement<BricksContainerView>(BricksContainerPrefab, GameObjectsContainer);
-            _brickViewInstance = InstantiateElement<BrickView>(BrickViewPrefab, _bricksContainerInstance.transform, new Vector3(0, 4f, 0));
+            _bricksContainerInstance = InstantiateElement<BricksContainerView>(BricksContainerPrefab, GameObjectsContainer, new Vector3(0, 2.5f, 0));
+            var brickTypes = Enum.GetValues(typeof(BrickType)).Cast<BrickType>().ToList();
+            for (var i = 0; i < brickTypes.Count; i++)
+            {
+                for (var j = 0; j < bricksCountPerType; j++)
+                {
+                    var brickViewInstance = InstantiateElement<BrickView>(BrickViewPrefab, _bricksContainerInstance.transform);
+                    _brickViewInstances.Add(brickViewInstance);
+                } 
+            }
 
             _racketViewInstance = InstantiateElement<RacketView>(RacketViewPrefab, _fieldViewInstance.gameObject.transform, new Vector3(0, -4f, 0));
 
@@ -53,7 +66,7 @@ namespace Managers
         {
             var instance = InstantiateElement<T>(prefab, parentContainer);
 
-            instance.transform.position = position;
+            instance.transform.localPosition = position;
 
             return instance;
         }
